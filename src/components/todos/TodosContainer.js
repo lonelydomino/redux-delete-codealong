6b6cfb0957f6dc5bb78876f4d540bedbdb22fd71
Michael Martinez <zeroeclipse@live.com>
2021-08-01 16:24:50 -0700

    commit tree caf48f8c75e1c5d3509ed6af982032637eefb90a

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -4,8 +4,7 @@ import Todo from './Todo'
 
 class TodosContainer extends Component {
 
-  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
-
+  renderTodos = () => this.props.todos.map((todo) => <Todo delete={this.props.delete} key={todo.id} text={todo} />)
   render() {
     return(
       <div>
@@ -20,5 +19,9 @@ const mapStateToProps = state => {
     todos: state.todos
   }
 }
-
-export default connect(mapStateToProps)(TodosContainer);
+const mapDispatchToProps = dispatch => {
+  return {
+    delete: todoText => dispatch({type: 'DELETE_TODO', payload: todoText })
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
